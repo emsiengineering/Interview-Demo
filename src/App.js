f20fbba76fd938bd76c9ea33bfbcb874efa3b01b
Ryan Lease <ryan.lease@economicmodeling.com>
2018-12-04 09:15:30 -0800

    Remove some unnecessary stuff; add a data file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <ul>
+          <li>Chief Executives</li>
+          <li>Chief Sustainability Officers</li>
+          <li>General and Operations Managers</li>
+          <li>Legislators</li>
+          <li>Advertising and Promotions Managers</li>
+          <li>Green Marketers</li>
+          <li>Marketing Managers</li>
+          <li>Sales Managers</li>
+          <li>Public Relations and Fundraising Managers</li>
+          <li>Administrative Services Managers</li>
+          <li>Computer and Information Systems Managers</li>
+          <li>Treasurers and Controllers</li>
+          <li>Financial Managers, Branch or Department</li>
+          <li>Industrial Production Managers</li>
+          <li>Quality Control Systems Managers</li>
+          <li>Geothermal Production Managers</li>
+          <li>Biofuels Production Managers</li>
+          <li>Biomass Power Plant Managers</li>
+          <li>Methane/Landfill Gas Collection System Operators</li>
+          <li>Hydroelectric Production Managers</li>
+          <li>Purchasing Managers</li>
+          <li>Transportation Managers</li>
+          <li>Storage and Distribution Managers</li>
+          <li>Logistics Managers</li>
+          <li>Compensation and Benefits Managers</li>
+          <li>Human Resources Managers</li>
+          <li>Training and Development Managers</li>
+          <li>Nursery and Greenhouse Managers</li>
+          <li>Farm and Ranch Managers</li>
+          <li>Aquacultural Managers</li>
+          <li>Construction Managers</li>
+          <li>Education Administrators, Preschool and Childcare Center/Program</li>
+          <li>Education Administrators, Elementary and Secondary School</li>
+          <li>Education Administrators, Postsecondary</li>
+          <li>Distance Learning Coordinators</li>
+          <li>Fitness and Wellness Coordinators</li>
+          <li>Architectural and Engineering Managers</li>
+          <li>Biofuels/Biodiesel Technology and Product Development Managers</li>
+          <li>Food Service Managers</li>
+          <li>Funeral Service Managers</li>
+          <li>Gaming Managers</li>
+          <li>Lodging Managers</li>
+          <li>Medical and Health Services Managers</li>
+          <li>Natural Sciences Managers</li>
+          <li>Clinical Research Coordinators</li>
+          <li>Water Resource Specialists</li>
+          <li>Postmasters and Mail Superintendents</li>
+          <li>Property, Real Estate, and Community Association Managers</li>
+          <li>Social and Community Service Managers</li>
+          <li>Emergency Management Directors</li>
+          <li>Regulatory Affairs Managers</li>
+          <li>Compliance Managers</li>
+          <li>Investment Fund Managers</li>
+        </ul>
       </div>
     );
   }
